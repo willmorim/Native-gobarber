8f7e51961fb891e2f5833a6c417c981e2bce071c
willmorim <pdbawilliam@gmail.com>
2020-02-03 23:18:14 -0300

    fist commit

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View } from 'react-native';
+
+import Routes from './routes'
+
+// import { Container } from './styles';
+
+export default function App() {
+  return (
+    <Routes />
+  );
+}
---
+++ src/pages/SignIn/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+
+// import { Container } from './styles';
+
+export default function SignIn() {
+  return (
+    <Text> oioio</Text>
+  );
+}
